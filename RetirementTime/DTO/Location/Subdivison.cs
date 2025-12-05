ec76a1e281955148c90550b8231828059ebbf21c
Samuel Hamann <shamann4242@gmail.com>
2025-12-05 18:52:59 -0500

    Basic layout with breadcrumb for beginner-guide

---
+++ RetirementTime/DTO/Location/Subdivison.cs
@@ -0,0 +1,8 @@
+namespace RetirementTime.DTO.Location;
+
+public class Subdivision
+{
+    public long Id { get; set; }
+    public string? Code { get; set; }
+    public required string Name { get; set; }
+}
