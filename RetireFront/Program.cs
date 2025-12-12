4f0dfde60badf747d8b653845aa47626543840f7
Samuel Hamann <shamann4242@gmail.com>
2025-12-11 19:11:15 -0500

    New project shell

--- RetireFront/Program.cs
+++ RetireFront/Program.cs
@@ -1,4 +1,4 @@
-using RetirementTime.Components;
+using RetireFront.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +15,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
